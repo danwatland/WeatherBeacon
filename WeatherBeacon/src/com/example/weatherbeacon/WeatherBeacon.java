134de060757e90d7935c279005514f41362e11c4
Dan Watland <danwatland@gmail.com>
2013-10-23 14:56:48 -0500

    Additional comments in code and cleaned up a couple of things.

--- WeatherBeacon/src/com/example/weatherbeacon/WeatherBeacon.java
+++ WeatherBeacon/src/com/example/weatherbeacon/WeatherBeacon.java
@@ -10,7 +10,6 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
@@ -24,28 +23,41 @@ import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnLongClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 
+/**
+ * Main activity for Weather Beacon program. Shows state of weather beacon
+ * (based on the difference between today and tomorrow's forecasts) and has
+ * options that pop up in a contextual menu when long pressing on the beacon
+ * image.
+ * 
+ * @author Dan
+ * 
+ */
 public class WeatherBeacon extends FragmentActivity {
+
+	private String[] menuOptions = new String[] { "Weather Beacon Key",
+			"Beacon Map" };
 
 	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		// TODO Change the following code to separate class that extends Asyncrhonous task.
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
 				.permitAll().build();
 		StrictMode.setThreadPolicy(policy);
 
 		InputStream is = null;
-		
+
 		HttpClient client = new DefaultHttpClient();
-		HttpGet httpget = new HttpGet("http://weather.yahooapis.com/forecastrss?w=2391446");
+		HttpGet httpget = new HttpGet(
+				"http://weather.yahooapis.com/forecastrss?w=2391446");
 
 		try {
 			HttpResponse response = client.execute(httpget);
@@ -56,77 +68,89 @@ public class WeatherBeacon extends FragmentActivity {
 			Log.e("log_tag", "Error in http connection " + e.toString());
 		}
 
+		// Parse the XML data retrieved from the Yahoo! Weather API.
 		WeatherParser wp = new WeatherParser(is);
+
 		View view = this.getWindow().getDecorView();
 		view.setBackgroundColor(Color.BLACK);
+
+		// Store beacon to a variable so we can manipulate later.
 		ImageView iv = (ImageView) findViewById(R.id.weatherbeacon);
 		int beacon;
-		
-		if (wp.getTomorrowForecast() - wp.getTodayForecast() > 4) {
-			beacon = R.drawable.weatherbeacon_red;
-		} else if (wp.getTomorrowForecast() - wp.getTodayForecast() < -4) {
-			beacon = R.drawable.weatherbeacon_white;
+
+		// Change weather beacon color based on the difference between today and
+		// tomorrow's forecasts.
+		final int CHANGE_TOLERANCE = 4;
+		if (wp.getTomorrowForecast() - wp.getTodayForecast() > CHANGE_TOLERANCE) {
+			beacon = R.drawable.weatherbeacon_red; // Tomorrow is warmer
+		} else if (wp.getTomorrowForecast() - wp.getTodayForecast() < -CHANGE_TOLERANCE) {
+			beacon = R.drawable.weatherbeacon_white; // Tomorrow is cooler
 		} else {
-			beacon = R.drawable.weatherbeacon_green;
+			beacon = R.drawable.weatherbeacon_green; // Tomorrow is about the
+														// same
 		}
-		
+
+		// Set image to color specified above.
 		iv.setImageResource(beacon);
-		
+
+		// Determine whether tomorrow's forecast includes precipitation. This is
+		// currently hardcoded as Yahoo's table for the values seems somewhat
+		// arbitrary at first glance.
 		int conditions = wp.getConditions();
-		
-		if ((conditions <= 18 || conditions >= 35) && conditions != 36 && conditions != 44) {
-			Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.tween);
+		if ((conditions <= 18 || conditions >= 35) && conditions != 36
+				&& conditions != 44) {
+			// If tomorrow's forecast includes precipitation, make beacon flash.
+			Animation fadeInAnimation = AnimationUtils.loadAnimation(this,
+					R.anim.tween);
 			iv.startAnimation(fadeInAnimation);
 		}
-		
-		registerForContextMenu(iv);
-		
-	}
-	
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		// Enable contextual menu on long press of beacon.
+		registerForContextMenu(iv);
+
 	}
-	
+
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
-		// TODO Auto-generated method stub
 		super.onCreateContextMenu(menu, v, menuInfo);
 		menu.setHeaderTitle("Options");
-		menu.add(Menu.NONE, 1, 1, "Weather Beacon Key");
-		menu.add(Menu.NONE, 2, 2, "Beacon Map");
+
+		// Add pre-defined options to contextual menu.
+		for (int i = 0; i < menuOptions.length; ++i) {
+			menu.add(Menu.NONE, i, i + 1, menuOptions[i]);
+		}
 	}
-	
+
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
-		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
-		
-		switch (item.getItemId()) {
-		case 1:
-			new AlertDialog.Builder(this)
-		    .setTitle("WEATHER BEACON KEY")
-		    .setMessage("Red: Warm weather ahead.\nWhite: Cold weather in sight.\nGreen: No change forseen.\nFlashing: Precipitation.")
-		    .setNeutralButton("Great.", new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int which) { 
-		            // continue with delete
-		        }
-		     })
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item
+				.getMenuInfo();
 
-		     .show();
+		// TODO Change this to switch on the titles of menu items instead of Item IDs.
+		switch (item.getItemId()) {
+		case 0:
+			new AlertDialog.Builder(this)
+					.setTitle("WEATHER BEACON KEY")
+					.setMessage(
+							"Red: Warm weather ahead.\nWhite: Cold weather in sight.\nGreen: No change forseen.\nFlashing: Precipitation.")
+					.setNeutralButton("Great.",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int which) {
+									// continue with delete
+								}
+							})
+
+					.show();
 			break;
-		case 2:
+		case 1:
 			Intent intent = new Intent(this, WeatherMap.class);
 			startActivity(intent);
 		default:
 			break;
 		}
-		
-		
-		Log.d("log_tag", "item.getItemId() == " + item.getItemId());
+
 		return true;
 	}
 
